65adeaa3c66332a8f93117879d589ddd3f30ce66
joaopresto <jguedespresto@gmail.com>
2018-04-18 14:17:22 -0300

    Criado Classe Cliente

---
+++ ProjetoLivroAberto/src/model/Cliente.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Posto Figueira
+ */
+public class Cliente {
+    
+    private String CPF;
+    private String nome;
+    private String estadoCivil;
+    private String telefone;
+    private String email;
+
+    public Cliente() {
+        
+    }
+
+    public Cliente(String CPF, String nome, String estadoCivil, String telefone, String email) {
+        this.CPF = CPF;
+        this.nome = nome;
+        this.estadoCivil = estadoCivil;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
